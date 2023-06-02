93929cc561d2472cae2082cb1a794615b4a3a0a3
shinnn <118076712+Xasigaki@users.noreply.github.com>
2023-06-02 16:43:11 +0000

    Delete shop_api directory

--- shop_api/product/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from product.models import Category, Product, Review, Tag
-
-# Register your models here.
-
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Review)
-admin.site.register(Tag)
--- shop_api/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('authorization/', views.AuthorizationAPIView.as_view()),
-    path('registration/', views.registration_api_views),
-    path('api/v1/users/confirm/', views.confirm_user_views)
-]
