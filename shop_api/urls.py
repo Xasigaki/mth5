8e3b48152383780c8fd81389b59cb40cbd6ae303
shinnn <118076712+Xasigaki@users.noreply.github.com>
2023-06-12 20:52:03 +0000

    homework 6-7

--- shop_api/urls.py
+++ shop_api/urls.py
@@ -3,16 +3,24 @@ from shop_api import settings
 from django.contrib import admin
 from django.urls import path, include
 from product import views
+from . import swagger
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/categories/', views.category_api_view),
-    path('api/v1/categories/<int:id>/', views.category_detail_api_view),
-    path('api/v1/products/', views.products_api_view),
-    path('api/v1/products/<int:id>/', views.product_detail_api_view),
-    path('api/v1/reviews/', views.reviews_api_view),
-    path('api/v1/reviews/<int:id>/', views.review_detail_api_view),
-    path('api/v1/products/reviews/', views.products_reviews_api_view),
+    path('api/v1/categories/', views.CategoryAPIView.as_view({'get': 'list', 'post': 'create'})),
+    path('api/v1/categories/<int:id>/', views.CategoryAPIView.as_view({'get': 'retrieve',
+                                                                       'put': 'update',
+                                                                       'delete': 'destroy'})),
+    path('api/v1/products/', views.ProductsAPIView.as_view({'get': 'list', 'post': 'create'})),
+    path('api/v1/products/<int:id>/', views.ProductsAPIView.as_view({'get': 'retrieve',
+                                                                     'put': 'update',
+                                                                     'delete': 'destroy'})),
+    path('api/v1/reviews/', views.ReviewsAPIView.as_view({'get': 'list', 'post': 'create'})),
+    path('api/v1/reviews/<int:id>/', views.ReviewsAPIView.as_view({'get': 'retrieve',
+                                                                   'put': 'update',
+                                                                   'delete': 'destroy'})),
+    path('api/v1/products/reviews/', views.ProductsReviewsAPIView.as_view()),
     path('api/users/', include('users.urls')),
 ]
+urlpatterns += swagger.urlpatterns
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
